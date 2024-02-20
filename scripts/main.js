37af3d178cd2bbe43c7e029f9dd63f0298c6fbc8
LuisF Patrocinio <patrocinioluisf@gmail.com>
2024-02-19 21:46:37 -0300

    Script comentado para animação do gif de perfil.

--- scripts/main.js
+++ scripts/main.js
@@ -1,68 +1,14 @@
-window.onload = main
-var personGrid = null;
+/*
+// SCRIPT COMENTADO PARA ANIMAR O GIF DE PERFIL AO PASSAR O MOUSE
+document.querySelectorAll('.member img').forEach(img => {
+    img.addEventListener('mouseenter', () => {
+        img.src = "assets/profileGif.gif";
+        img.style.animationIterationCount = '1';
+    });
 
-const alunosCadastrados = [];
-
-async function main() {
-    personGrid = document.getElementById('people-grid');
-    console.log(personGrid)
-    var alunos = null;
-    try {
-        alunos = await getAlunos();
-        console.log(alunos);
-    } catch (error) {
-        alert(error.message);
-        return;
-    }
-
-    for(i = 0; i < alunos.length; i++){
-        var aluno = alunos[i];
-        if (aluno in alunosCadastrados) return;
-        criarAluno(aluno);
-    }
-}
-
-
-async function criarAluno(aluno) {
-    const alunoCard = createElementWithClassName('div', 'person-card');
-    const cardHeader = createElementWithClassName('div', 'card-header');
-    const cardBody = createElementWithClassName('div', 'card-body');
-
-    personGrid.appendChild(alunoCard);
-    alunoCard.appendChild(cardHeader);
-    alunoCard.appendChild(cardBody);
-
-    const fotoAluno = aluno.fotoUrl ? aluno.fotoUrl : 'https://www.w3schools.com/howto/img_avatar.png';
-    const dataNasc = aluno.dataNasc ? aluno.dataNasc : 'não informada';
-
-    cardHeader.innerHTML = `<img src="${fotoAluno}" alt="Foto de ${aluno.nome}" class="person-photo">  <h2>${aluno.nome}</h2>`;
-    cardBody.innerHTML = `<p>Data de nascimento: ${formatarDAtaDeNascimento(dataNasc)} </p>  <p> Sobre: ${aluno.descricao} </p>`;
-}
-
-
-function formatarDAtaDeNascimento(data) {
-    const dataNasc = new Date(data);
-    const dia = String(dataNasc.getDate()).padStart(2, '0')
-    const mes = String(dataNasc.getMonth() + 1).padStart(2, '0');
-    return `${dia}/${mes}/${dataNasc.getFullYear()}`;
-}
-
-
-
-function createElementWithClassName(tagName, className) { 
-    const element = document.createElement(tagName);
-    element.className = className;
-    return element;
-}
-
-
-async function getAlunos() {
-    var alunos = [];
-    await fetch("http://localhost:8080/alunos").catch(() => {throw new Error("Não foi possível conectar a API")}).then(response => {
-        if(response.ok){
-            return response.json();
-        }
-        throw new Error('Erro ao buscar alunos');
-    }).then(data => {alunos = data;});
-    return alunos;
-}
+    // Reiniciar a animação ao sair do mouse
+    img.addEventListener('mouseleave', () => {
+        img.src = "assets/profileFirstFrame.gif"
+    });
+});
+*/
